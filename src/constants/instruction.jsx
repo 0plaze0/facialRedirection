d7d2ea3831c34ef023b8477933b88ab1db306464
plaze <banikakash131@gmail.com>
2024-02-25 19:16:51 +0530

    feat: add instruction

---
+++ src/constants/instruction.jsx
@@ -0,0 +1,50 @@
+export const instructions = [
+  {
+    id: 1,
+    instruction: "Sit in a Well-Lit Area",
+    description:
+      "Ensure that you are sitting in a well-lit room or area so that your facial features are clearly visible to the webcam.",
+  },
+  {
+    id: 2,
+    instruction: "Position Yourself in Front of the Webcam",
+    description:
+      "Sit directly facing the webcam so that your face is centered within the camera frame.",
+  },
+  {
+    id: 3,
+    instruction: "Maintain a Comfortable Distance",
+    description:
+      "Sit at a comfortable distance from the webcam where your face is clearly visible but not too close that it distorts the image.",
+  },
+  {
+    id: 4,
+    instruction: "Keep Your Back Straight",
+    description:
+      "Maintain good posture by sitting with your back straight and shoulders relaxed.",
+  },
+  {
+    id: 5,
+    instruction: "Face the Camera Directly",
+    description:
+      "Make sure that your face is not tilted or turned away from the camera. Keep your head straight and facing forward.",
+  },
+  {
+    id: 6,
+    instruction: "Avoid Obstructions",
+    description:
+      "Ensure that there are no objects blocking your face or the webcam lens. Keep the area around the webcam clear.",
+  },
+  {
+    id: 7,
+    instruction: "Follow On-Screen Instructions",
+    description:
+      "Pay attention to any on-screen prompts or instructions that guide you on how to adjust your sitting position for optimal facial recognition.",
+  },
+  {
+    id: 8,
+    instruction: "Relax and Be Patient",
+    description:
+      "Stay relaxed and patient as the application verifies your sitting position. It may take a few moments to process and validate your facial traits.",
+  },
+];
